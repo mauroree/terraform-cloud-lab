b05370ac4981d0639d829d9dd44a44b9400d4ef7
Mauro <mauroree@gmail.com>
2026-01-09 15:52:30 -0300

    infra terraform com nginx

---
+++ main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+  
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = var.instance_name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  description = "Região da AWS"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "instance_type" {
+  description = "Tipo da EC2"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_name" {
+  description = "Nome da EC2"
+  type        = string
+  default     = "terraform-web-lab"
+}
+
+variable "key_name" {
+  description = "Nome do Key Pair na AWS"
+  type        = string
+}
+
+variable "allowed_ip" {
+  description = "IP permitido para SSH"
+  type        = string
+}
