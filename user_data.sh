805298fae7f83db5e785268744eebed5f57e2e98
Mauro <mauroree@gmail.com>
2026-01-12 15:04:39 -0300

    feat: deploy formulario web via docker hub

--- user_data.sh
+++ user_data.sh
@@ -7,4 +7,14 @@ apt-get install -y docker.io
 systemctl start docker
 systemctl enable docker
 
-docker run -d -p 80:80 --name nginx nginx
+usermod -aG docker ubuntu
+
+docker pull mrxjr/formulario-web:latest
+
+docker rm -f formulario || true
+
+docker run -d \
+  -p 80:80 \
+  --name formulario \
+  mrxjr/formulario-web:latest
+
